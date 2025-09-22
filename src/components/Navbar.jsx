161e2ad7956f8d7a90cf69d7c0c0d1c49df83c7a
devvrat-singh-gth <devvratsinghbanga2005@gmail.com>
2025-09-22 20:29:03 +0530

    Proto2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -190,15 +190,16 @@ const Navbar = () => {
                                border border-gray-200 dark:border-gray-700 rounded-lg shadow-lg 
                                overflow-hidden z-50"
                   >
-                    <Link
-                      to="/profile"
-                      onClick={() => setShowDropdown(false)}
-                      className="w-full text-left px-4 py-2 text-sm text-gray-700 dark:text-gray-200 
-             hover:bg-gray-100 dark:hover:bg-gray-700 transition block"
+                    <button
+                      onClick={() => {
+                        setShowDropdown(false);
+                        navigate("/profile");
+                      }}
+                      className="w-full text-left px-4 py-3 text-sm text-gray-700 dark:text-gray-200
+          hover:bg-gray-100 dark:hover:bg-gray-700 transition block"
                     >
                       Profile
-                    </Link>
-
+                    </button>
                     <button
                       onClick={handleLogout}
                       className="w-full text-left px-4 py-2 text-sm text-red-600 dark:text-red-400 
