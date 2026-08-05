007b4ff934599e94b541df2d16a08c2d84db3dbb
DEVVRAT SINGH <devvratsinghbanga2005@gmail.com>
2026-08-05 19:33:54 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -88,7 +88,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="border-t border-gray-200 dark:border-gray-700 py-4 text-center text-gray-600 dark:text-gray-400">
-          <p>&copy; 2025 InternshipHub. All rights reserved.</p>
+          <p>&copy; 2026 InternAdda. All rights reserved.</p>
         </div>
       </div>
     </footer>
